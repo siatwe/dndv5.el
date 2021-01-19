2df34069f067677d52e8fb1309553a3f9ce02cae
Simon Weis <simonattilaweis@tutanota.com>
2021-01-19 23:05:21 +0100

    create README

--- dndv5.el
+++ dndv5.el
@@ -8,34 +8,46 @@
 ;;; VARIABLES
 ;;;
 
-;;; General
-(defvar dndv5/general-name "Melbor" "The name of your character")
-(defvar dndv5/general-race "Elf" "The race of your character")
-(defvar dndv5/general-background "Einsiedler" "The background of your character")
+;;; General (writeable)
+(defvar dndv5/w-general-name "Melbor" "The name of your character")
+(defvar dndv5/w-general-race "Elf" "The race of your character")
+(defvar dndv5/w-general-background "Einsiedler" "The background of your character")
 
-;;; Base attributes
-(defvar dndv5/attributes-base-strength      8 "The strength of your character")
-(defvar dndv5/attributes-base-dexterity    14 "The dexterity of your character")
-(defvar dndv5/attributes-base-constitution 13 "The constitution of your character")
-(defvar dndv5/attributes-base-intelligence 16 "The intelligence of your character")
-(defvar dndv5/attributes-base-wisdom       12 "The wisom of your character")
-(defvar dndv5/attributes-base-charisma     10 "The charisma of your character")
+;;; Base attributes (writeable)
+(defvar dndv5/w-attributes-base-strength      8 "The strength of your character")
+(defvar dndv5/w-attributes-base-dexterity    14 "The dexterity of your character")
+(defvar dndv5/w-attributes-base-constitution 13 "The constitution of your character")
+(defvar dndv5/w-attributes-base-intelligence 16 "The intelligence of your character")
+(defvar dndv5/w-attributes-base-wisdom       12 "The wisom of your character")
+(defvar dndv5/w-attributes-base-charisma     10 "The charisma of your character")
 
-;;; Boni/Mali on attributes by race
-(defvar dndv5/attributes-bonimali-strength     0 "The boni/mali strength of your character by your race")
-(defvar dndv5/attributes-bonimali-dexterity    2 "The dexterity of your character by your race")
-(defvar dndv5/attributes-bonimali-constitution 0 "The constitution of your character by your race")
-(defvar dndv5/attributes-bonimali-intelligence 2 "The intelligence of your character by your race")
-(defvar dndv5/attributes-bonimali-wisdom       0 "The wisom of your character by your race")
-(defvar dndv5/attributes-bonimali-charisma     0 "The charisma of your character by your race")
+;;; Boni/Mali on attributes by race (writeable)
+(defvar dndv5/w-attributes-bonimali-strength     0 "The boni/mali strength of your character by your race")
+(defvar dndv5/w-attributes-bonimali-dexterity    2 "The dexterity of your character by your race")
+(defvar dndv5/w-attributes-bonimali-constitution 0 "The constitution of your character by your race")
+(defvar dndv5/w-attributes-bonimali-intelligence 2 "The intelligence of your character by your race")
+(defvar dndv5/w-attributes-bonimali-wisdom       0 "The wisom of your character by your race")
+(defvar dndv5/w-attributes-bonimali-charisma     0 "The charisma of your character by your race")
 
-;;; Total attributes
-(defvar dndv5/attributes-total-strength (+ dndv5/attributes-base-strength dndv5/attributes-bonimali-strength) "The total strength of your character")
-(defvar dndv5/attributes-total-dexterity (+ dndv5/attributes-base-dexterity dndv5/attributes-bonimali-dexterity) "The total dexterity of your character")
-(defvar dndv5/attributes-total-constitution (+ dndv5/attributes-base-constitution dndv5/attributes-bonimali-constitution) "The total constitution of your character")
-(defvar dndv5/attributes-total-intelligence (+ dndv5/attributes-base-intelligence dndv5/attributes-bonimali-intelligence) "The total intelligence of your character")
-(defvar dndv5/attributes-total-wisdom (+ dndv5/attributes-base-wisdom dndv5/attributes-bonimali-wisdom) "The total wisdom of your character")
-(defvar dndv5/attributes-total-charisma (+ dndv5/attributes-base-charisma dndv5/attributes-bonimali-charisma) "The total charisma of your character")
+;;; Total attributes (readonly)
+(defvar dndv5/r-attributes-total-strength
+  (+ dndv5/attributes-base-strength dndv5/attributes-bonimali-strength)
+  "The total strength of your character")
+(defvar dndv5/r-attributes-total-dexterity
+  (+ dndv5/attributes-base-dexterity dndv5/attributes-bonimali-dexterity)
+  "The total dexterity of your character")
+(defvar dndv5/r-attributes-total-constitution
+  (+ dndv5/attributes-base-constitution dndv5/attributes-bonimali-constitution)
+  "The total constitution of your character")
+(defvar dndv5/r-attributes-total-intelligence
+  (+ dndv5/attributes-base-intelligence dndv5/attributes-bonimali-intelligence)
+  "The total intelligence of your character")
+(defvar dndv5/r-attributes-total-wisdom
+  (+ dndv5/attributes-base-wisdom dndv5/attributes-bonimali-wisdom)
+  "The total wisdom of your character")
+(defvar dndv5/r-attributes-total-charisma
+  (+ dndv5/attributes-base-charisma dndv5/attributes-bonimali-charisma)
+  "The total charisma of your character")
 
 ;;;
 
